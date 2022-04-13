4861a3a680304bf4ebc85ae0305db0eccfa5f733
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-13 23:39:55 +0500

    blog updated

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,12 +13,7 @@ class BlogsController < ApplicationController
     end
 
     def create 
-        @blog = Blog.new(
-            title: params[:blog][:title],
-            content: params[:blog][:content],
-            author: params[:blog][:author],
-            date:Time.now,
-        )
+        @blog = Blog.new(blogs_params.merge({date: Time.now}))
         if @blog.save
             redirect_to blogs_path 
         else
@@ -28,6 +23,20 @@ class BlogsController < ApplicationController
 
     def edit 
         @blog = Blog.find(params[:id])
+    end
+
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog.update(blogs_params.merge({date: Time.now}))
+            redirect_to blogs_path
+        else
+            render :update
+        end
+    end
+
+    private 
+    def blogs_params 
+        params.require(:blog).permit(:title,:content,:author)
     end
 
 end
