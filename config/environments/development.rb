1bbd443f82529be84060b919bbb3eea4b61b433c
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-14 12:08:13 +0500

    send password reset email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+ 
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -31,7 +33,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true 
 
   config.action_mailer.perform_caching = false
 
@@ -60,4 +62,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+   #GMAIL SMTP
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address:              'smtp.gmail.com',
+     port:                 587,
+     domain:               'localhost:3000',
+     user_name:            'abdur.rehman@devsinc.com',
+     password:             'dhhqzffbrlymrqzc',
+     authentication:       'plain',
+     enable_starttls_auto: true }
 end
