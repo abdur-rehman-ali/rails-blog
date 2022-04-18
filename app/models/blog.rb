fefff71adca58b58331c6da35b105413d9a17ae7
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-18 09:10:19 +0500

    added custom validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,13 @@
 class Blog < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+    validate :title_cannot_be_ali 
     belongs_to :user
+
+
+    def title_cannot_be_ali
+        if title=="ali"
+            errors.add(:title,'Title cannot be ali')
+        end
+    end
 end
